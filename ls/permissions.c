34ab443e7d9bef2effd8db720c3a5fbcf4dbe835
RazikaBengana <razikabengana@gmail.com>
2024-04-20 22:44:39 +0200

    Add `permissions` function to mimic `ls -l` output, decoding file permissions using `struct stat`

---
+++ ls/permissions.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * permissions - program that prints file permissions like 'ls -l'
+ *
+ * this function takes a struct stat with file info and prints its permissions
+ *
+ * @file_stat: struct with file information
+ *
+ * Return: nothing (void)
+ */
+
+void permissions(struct stat file_stat)
+{
+    printf((S_ISDIR(file_stat.st_mode)) ? "d" : "-");
+    printf((file_stat.st_mode & S_IRUSR) ? "r" : "-");
+    printf((file_stat.st_mode & S_IWUSR) ? "w" : "-");
+    printf((file_stat.st_mode & S_IXUSR) ? "x" : "-");
+    printf((file_stat.st_mode & S_IRGRP) ? "r" : "-");
+    printf((file_stat.st_mode & S_IWGRP) ? "w" : "-");
+    printf((file_stat.st_mode & S_IXGRP) ? "x" : "-");
+    printf((file_stat.st_mode & S_IROTH) ? "r" : "-");
+    printf((file_stat.st_mode & S_IWOTH) ? "w" : "-");
+    printf((file_stat.st_mode & S_IXOTH) ? "x" : "-");
+}
