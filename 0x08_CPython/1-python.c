84ca61c8225d32277c5199136cff9d639c097159
RazikaBengana <razikabengana@gmail.com>
2024-04-22 03:30:15 +0200

    Correct the indentation in `1-python.c`

--- 0x08_CPython/1-python.c
+++ 0x08_CPython/1-python.c
@@ -16,16 +16,16 @@
 
 void print_python_list(PyObject *p)
 {
-    int i;
+	int i;
 
-    printf("[*] Python list info\n");
+    	printf("[*] Python list info\n");
 
-    printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
+    	printf("[*] Size of the Python List = %lu\n", Py_SIZE(p));
 
-    printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+    	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
 
-    for (i = 0; i < Py_SIZE(p); i++)
-    {
-        printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
-    }
+    	for (i = 0; i < Py_SIZE(p); i++)
+    	{
+        	printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+    	}
 }
