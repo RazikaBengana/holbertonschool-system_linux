def43e6505bf0ee94cee92a542f13afadd08a3f8
RazikaBengana <razikabengana@gmail.com>
2024-04-22 03:30:38 +0200

    Correct the indentation in `3-python.c`

--- 0x08_CPython/3-python.c
+++ 0x08_CPython/3-python.c
@@ -17,36 +17,36 @@
 
 void print_python_bytes(PyObject *p)
 {
-    int i;
-    Py_ssize_t size, printed_bytes;
-    char *array_as_string = NULL;
+	int i;
+	Py_ssize_t size, printed_bytes;
+	char *array_as_string = NULL;
 
-    printf("[.] bytes object info\n");
+	printf("[.] bytes object info\n");
 
-    if (!PyBytes_Check(p))
-    {
-        printf("  [ERROR] Invalid Bytes Object\n");
-        fflush(stdout);
-        return;
-    }
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		fflush(stdout);
+		return;
+	}
 
-    size = ((PyVarObject *)(p))->ob_size;
+	size = ((PyVarObject *)(p))->ob_size;
 
-    printf("  size: %li\n", size);
+	printf("  size: %li\n", size);
 
-    array_as_string = ((PyBytesObject *)(p))->ob_sval;
+	array_as_string = ((PyBytesObject *)(p))->ob_sval;
 
-    printf("  trying string: %s\n", array_as_string);
+	printf("  trying string: %s\n", array_as_string);
 
-    printed_bytes = (size + 1 >= 10) ? 10 : size + 1;
+	printed_bytes = (size + 1 >= 10) ? 10 : size + 1;
 
-    printf("  first %li bytes:", printed_bytes);
+	printf("  first %li bytes:", printed_bytes);
 
-    for (i = 0; i < printed_bytes; i++)
-        printf(" %02x", (unsigned char)(array_as_string[i]));
-    putchar('\n');
+	for (i = 0; i < printed_bytes; i++)
+		printf(" %02x", (unsigned char)(array_as_string[i]));
+	putchar('\n');
 
-    fflush(stdout);
+	fflush(stdout);
 }
 
 
@@ -64,25 +64,25 @@ void print_python_bytes(PyObject *p)
 
 void print_python_float(PyObject *p)
 {
-    char float_str[40];
+	char float_str[40];
 
-    printf("[.] float object info\n");
+	printf("[.] float object info\n");
 
-    if (!PyFloat_Check(p))
-    {
-        printf("  [ERROR] Invalid Float Object\n");
-        fflush(stdout);
-        return;
-    }
+	if (!PyFloat_Check(p))
+	{
+		printf("  [ERROR] Invalid Float Object\n");
+		fflush(stdout);
+		return;
+	}
 
-    sprintf(float_str, "%.16g", ((PyFloatObject *)p)->ob_fval);
+	sprintf(float_str, "%.16g", ((PyFloatObject *)p)->ob_fval);
 
-    if (strchr(float_str, '.') != NULL)
-	    printf("  value: %s\n", float_str);
-    else
-        printf("  value: %.1f\n", ((PyFloatObject *)p)->ob_fval);
+	if (strchr(float_str, '.') != NULL)
+		printf("  value: %s\n", float_str);
+	else
+		printf("  value: %.1f\n", ((PyFloatObject *)p)->ob_fval);
 
-    fflush(stdout);
+	fflush(stdout);
 }
 
 
@@ -100,38 +100,38 @@ void print_python_float(PyObject *p)
 
 void print_python_list(PyObject *p)
 {
-    int i;
-    Py_ssize_t size;
-    PyObject *list_member;
+	int i;
+	Py_ssize_t size;
+	PyObject *list_member;
 
-    printf("[*] Python list info\n");
+	printf("[*] Python list info\n");
 
-    if (!PyList_Check(p))
-    {
-        printf("  [ERROR] Invalid List Object\n");
-        fflush(stdout);
-        return;
-    }
+	if (!PyList_Check(p))
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		fflush(stdout);
+		return;
+	}
 
-    size = ((PyVarObject *)(p))->ob_size;
+	size = ((PyVarObject *)(p))->ob_size;
 
-    printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Size of the Python List = %li\n", size);
 
-    printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
 
-    for (i = 0; i < size; i++)
-    {
-        list_member = ((PyListObject *)p)->ob_item[i];
-        printf("Element %d: %s\n", i,
-               list_member->ob_type->tp_name);
+	for (i = 0; i < size; i++)
+	{
+		list_member = ((PyListObject *)p)->ob_item[i];
+		printf("Element %d: %s\n", i,
+		       list_member->ob_type->tp_name);
 
-        if (PyBytes_Check(list_member))
-            print_python_bytes(list_member);
+		if (PyBytes_Check(list_member))
+			print_python_bytes(list_member);
 
-        else if (PyFloat_Check(list_member))
-            print_python_float(list_member);
+		else if (PyFloat_Check(list_member))
+			print_python_float(list_member);
 
-    }
+	}
 
-    fflush(stdout);
+	fflush(stdout);
 }
